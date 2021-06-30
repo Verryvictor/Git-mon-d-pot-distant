eef042b49fe5a94984d56f34f215d02687eae919
Verry Victor <verry.victor@gmail.com>
2021-06-30 09:34:45 +0200

    Tp Crud

--- Victor2/BACK/SERVEUR/PHP/contact.php
+++ Victor2/BACK/SERVEUR/PHP/contact.php
@@ -1,25 +1,30 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Jarditou</title>
-</head>
-<body>
-    <div class="container">
 
-        <img src="jarditou_html_zip/images/jarditou_logo.jpg" alt="Logo Jarditou" title="Logo Jarditou" width="210" height="70">
-    
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0 shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>Jarditou</title>
+</head>
+
+<body>
+  <div class="container">
+
+    <img src="jarditou_html_zip/images/jarditou_logo.jpg" alt="Logo Jarditou" title="Logo Jarditou" width="210"
+      height="70">
+
     <h1 class="display-5 float-right">Tout le jardin</h1>
-    
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">Jarditou.com</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a class="nav-link" href="Index.html">Accueil <span class="sr-only">(current)</span></a>
@@ -37,110 +42,122 @@
         </form>
       </div>
     </nav>
-    
+
     <img src="jarditou_html_zip/images/promotion.jpg" alt="Promotions" title="Promotions" width="100%">
-  
+
     <form action="script.php" method="POST">
-    <p>* Ces zones sont obligatoires </p>
-    <br>
-        <legend><p class="h3">Vos coordonnées</p></legend>
+      <p>* Ces zones sont obligatoires </p>
+      <br>
+      <legend>
+        <p class="h3">Vos coordonnées</p>
+      </legend>
 
-        <div class="form-group">
-            <label for="nom">Votre nom* :</label>
-            <input class="form-control" type="text" id="nom" name="nom">
-        </div>
-
-        
-
-        <div class="form-group">
-            <label for="prènom">Votre prènom* :</label>
-            <input class="form-control" type="text" id="prènom" name="prènom">
-        </div>
-
-    
-
-        <div class="form-group">
-            <label for="sexe">Sexe* :</label>
-            <input type="radio" id="sexe" name="sexe" value="Féminin">Féminin
-            <input type="radio" name="sexe" value="Masculin">Masculin
-            <input type="radio" name="sexe" value="Neutre">Neutre
-        </div>
-
-        
-        <div class="form-group">
-            <label for="ddn">Date de naissance* :</label>
-            <input class="form-control" type="date" id="ddn" name="ddn" >
-        </div>
-       
-        <div class="form-group">
-            <label for="cp">Code postal* :</label>
-            <input class="form-control" type="number" id="cp" name="cp" >
-        </div>
-       
-        <div class="form-group">
-            <label for="adresse">Adresse :</label>
-            <input class="form-control" type="text" id="adresse" name="adresse">
-        </div>
-       
-        <div class="form-group">
-            <label for="ville">Ville :</label>
-            <input class="form-control" type="text" id="ville" name="ville">
-        </div>
-       
-        <div class="form-group">
-            <label for="email">Email* :</label>
-            <input class="form-control" type="email" id="email" name="email" placeholder="dave.loper@afpa.fr" >
-        </div>
+      <div class="form-group">
+        <label for="nom">Votre nom* :</label>
+        <input class="form-control" type="text" id="nom" name="nom">
+      </div>
 
 
-        <legend><p class="h3">Votre demande</p></legend>
 
-        <div class="form-group">
+      <div class="form-group">
+        <label for="prènom">Votre prènom* :</label>
+        <input class="form-control" type="text" id="prènom" name="prènom">
+      </div>
+
+
+
+      <div class="form-group">
+        <label for="sexe">Sexe* :</label>
+        <input type="radio" id="sexe" name="sexe" value="Féminin">Féminin
+        <input type="radio" name="sexe" value="Masculin">Masculin
+        <input type="radio" name="sexe" value="Neutre">Neutre
+      </div>
+
+
+      <div class="form-group">
+        <label for="ddn">Date de naissance* :</label>
+        <input class="form-control" type="date" id="ddn" name="ddn">
+      </div>
+
+      <div class="form-group">
+        <label for="cp">Code postal* :</label>
+        <input class="form-control" type="number" id="cp" name="cp">
+      </div>
+
+      <div class="form-group">
+        <label for="adresse">Adresse :</label>
+        <input class="form-control" type="text" id="adresse" name="adresse">
+      </div>
+
+      <div class="form-group">
+        <label for="ville">Ville :</label>
+        <input class="form-control" type="text" id="ville" name="ville">
+      </div>
+
+      <div class="form-group">
+        <label for="email">Email* :</label>
+        <input class="form-control" type="email" id="email" name="email" placeholder="dave.loper@afpa.fr">
+      </div>
+
+
+      <legend>
+        <p class="h3">Votre demande</p>
+      </legend>
+
+      <div class="form-group">
         <label for="sujet">Sujet* :</label>
-        <select class="form-control" id="sujet" name="sujet" >
-            <option value="" selected disabled>Veuillez séléctionner un sujet</option>
-            <option value="Commandes">Mes commandes</option>
-            <option value="Questions">Questions sur un produit</option>
-            <option value="Réclamation">Réclamations</option>
-            <option value="Autres">Autres</option>
+        <select class="form-control" id="sujet" name="sujet">
+          <option value="" selected disabled>Veuillez séléctionner un sujet</option>
+          <option value="Commandes">Mes commandes</option>
+          <option value="Questions">Questions sur un produit</option>
+          <option value="Réclamation">Réclamations</option>
+          <option value="Autres">Autres</option>
         </select>
-        </div>
+      </div>
 
-        <div class="form-group">
-            <label for="questions">Votre question* :</label>
-            <textarea class="form-control" id="questions" name="questions"></textarea>
-        </div>
-    
-        <div class="form-group">
-            <input type="checkbox" id="traitement" name="traitement">
-            <label for="traitement">* J'accepte le traitement informatique de ce formulaire</label>
-        </div>
-    
-    <button class="btn btn-dark" type="submit" id="envoyer" name="envoyer" value="Envoyer">Envoyer</button>
-    <button class="btn btn-dark" type="reset" id="annuler" name="annuler" value="Annuler">Annuler</button>
+      <div class="form-group">
+        <label for="questions">Votre question* :</label>
+        <textarea class="form-control" id="questions" name="questions"></textarea>
+      </div>
+
+      <div class="form-group">
+        <input type="checkbox" id="traitement" name="traitement">
+        <label for="traitement">* J'accepte le traitement informatique de ce formulaire</label>
+      </div>
+
+      <button class="btn btn-dark" type="submit" id="envoyer" name="envoyer" value="Envoyer">Envoyer</button>
+      <button class="btn btn-dark" type="reset" id="annuler" name="annuler" value="Annuler">Annuler</button>
     </form>
     <br>
     <footer class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a class="nav-link" href="#">mentions légales <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">horaires</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">plan du site</a>
-              </li>
-          </ul>
-        </div>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
+        aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#">mentions légales <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">horaires</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">plan du site</a>
+          </li>
+        </ul>
+      </div>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+  </div>
 </body>
+
 </html>
--- Victor2/BACK/SERVEUR/PHP/exo PHP.php
+++ Victor2/BACK/SERVEUR/PHP/exo PHP.php
@@ -1,156 +1,157 @@
-<?php
-/* - Exercice 1 - Boucles
+< ? php
+    /* - Exercice 1 - Boucles
 
-$a ;
-for ($a=0; $a<=150; $a = $a+2)
-{
-    echo $a . '<br>';
-}
-*/
-
-/* - Exercice 2 - Boucles
-
-$i ;
-for ($i=1; $i<=500; $i++)
-{
-    echo $i . ". Je dois savoir faire des sauvegardes régulières de mes fichiers. <br>";
-}
-*/
-
-/* - Exercice 3 - Boucles
-$tableau = '<table border="1"><tr>';
-
-    for($i =-1; $i <=12; $i++){
-        if($i >= 0){
-        $tableau .= "<th>".$i."</th>";
-        }
-        else{
-            $tableau .= "<th></th>";
-        };
-};
-
-  for($a = 0; $a <=12; $a++){
-        $tableau .= '<tr>';
-        $tableau .= "<th>".$a."</th>";
-            for ($b=0; $b <=12; $b++){
-                $tableau .= "<td>".$a*$b."</td>";
-            };
-        $tableau .= '</tr>';
-        };
-        $tableau .= '</table>';
-
-
-
-  $tableau .= '<tr><td></td></tr>';
-  $tableau .= '</table>';
-  echo $tableau;
- 
-echo table(12, 12);
-*/
-
-
-/* - Exercice 4 - Calculator
-$resultat = 0 ;
-function calculator ($nbr1, $operateur, $nbr2)
-{
-    if ($operateur = "+")
+    $a ;
+    for ($a=0; $a<=150; $a = $a+2)
     {
-        $resultat = $nbr1 + $nbr2 ;
+        echo $a . '<br>';
     }
-    else if ($operateur = "-")
+    */
+
+    /* - Exercice 2 - Boucles
+
+    $i ;
+    for ($i=1; $i<=500; $i++)
     {
-        $resultat = $nbr1 - $nbr2 ;
+        echo $i . ". Je dois savoir faire des sauvegardes régulières de mes fichiers. <br>";
     }
-    else if ($operateur = "*")
-    {
-        $resultat = $nbr1 * $nbr2 ;
-    }
-    else if ($operateur = "/")
-    {
-        $resultat = $nbr1 / $nbr2 ;
-    }
-    return $resultat ;
-}
-echo calculator (4, "*", 4) ;
-*/
+    */
 
-/* - Exercice 5 - Fichiers
-var-dump($_FILES);
-*/
+    /* - Exercice 3 - Boucles
+    $tableau = '<table border="1"><tr>';
 
-/*
-$a = array(
-    "19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "", "", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", "Validation"), 
-    "19002" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", ""), 
-    "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
-   );
-*/
-
-
-/* - Exercice 1 - Tableau
-    
-$valid = $a["19002"] ;
-$i = 0 ;
-       foreach($valid as $sub_valid)
-       {
-           $i++ ;
-           if($sub_valid == "Validation")
-           {
-               echo "La semaine de validation pour le 2ème groupe est la $i<sup>ème</sup> semaine.";
-               break ;
-           }
-        }
-*/
-
-
-
-/* - Exercice 2 - Tableau 
-
-$der = $a["19001"] ;
-$i = 0 ;
-foreach ($der as $sub_der)
-{
-    $i++ ;
-    if ($sub_der == "Validation")
-    {
-        $i-- ;
-        echo "La dernière semaine de stage est au bout de la $i <sup>ème</sup> semaine." ;
-        break ;
-    }
-}
-*/
-
-
-
-/* - Exercice 3 - Tableau
-
-$a2 = array();
-
-    foreach ($a as $index => $valeur)
-    {
-        $a2[] = $index;
-    }
-
-    echo "<pre>";
-    print_r($a2);
-    echo "</pre>";
-*/
-
-
-/* - Exercice 4 - Tableau
-
-$stage = $a["19003"];
-$i = 0 ;
-        foreach($stage as $sub_stage)
-        {
-            if($sub_stage == "Stage")
-            {
-                $i++ ;
+        for($i =-1; $i <=12; $i++){
+            if($i >= 0){
+            $tableau .= "<th>".$i."</th>";
             }
+            else{
+                $tableau .= "<th></th>";
+            };
+    };
+
+      for($a = 0; $a <=12; $a++){
+            $tableau .= '<tr>';
+            $tableau .= "<th>".$a."</th>";
+                for ($b=0; $b <=12; $b++){
+                    $tableau .= "<td>".$a*$b."</td>";
+                };
+            $tableau .= '</tr>';
+            };
+            $tableau .= '</table>';
+
+
+
+      $tableau .= '<tr><td></td></tr>';
+      $tableau .= '</table>';
+      echo $tableau;
+     
+    echo table(12, 12);
+    */
+
+
+    /* - Exercice 4 - Calculator
+    $resultat = 0 ;
+    function calculator ($nbr1, $operateur, $nbr2)
+    {
+        if ($operateur = "+")
+        {
+            $resultat = $nbr1 + $nbr2 ;
         }
-echo $i ;
-*/
+        else if ($operateur = "-")
+        {
+            $resultat = $nbr1 - $nbr2 ;
+        }
+        else if ($operateur = "*")
+        {
+            $resultat = $nbr1 * $nbr2 ;
+        }
+        else if ($operateur = "/")
+        {
+            $resultat = $nbr1 / $nbr2 ;
+        }
+        return $resultat ;
+    }
+    echo calculator (4, "*", 4) ;
+    */
+
+    /* - Exercice 5 - Fichiers
+    var-dump($_FILES);
+    */
+
+    /*
+    $a = array(
+        "19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "", "", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", "Validation"), 
+        "19002" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", ""), 
+        "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
+       );
+    */
+
+
+    /* - Exercice 1 - Tableau
+        
+    $valid = $a["19002"] ;
+    $i = 0 ;
+           foreach($valid as $sub_valid)
+           {
+               $i++ ;
+               if($sub_valid == "Validation")
+               {
+                   echo "La semaine de validation pour le 2ème groupe est la $i<sup>ème</sup> semaine.";
+                   break ;
+               }
+            }
+    */
 
 
 
-?>
+    /* - Exercice 2 - Tableau 
+
+    $der = $a["19001"] ;
+    $i = 0 ;
+    foreach ($der as $sub_der)
+    {
+        $i++ ;
+        if ($sub_der == "Validation")
+        {
+            $i-- ;
+            echo "La dernière semaine de stage est au bout de la $i <sup>ème</sup> semaine." ;
+            break ;
+        }
+    }
+    */
+
+
+
+    /* - Exercice 3 - Tableau
+
+    $a2 = array();
+
+        foreach ($a as $index => $valeur)
+        {
+            $a2[] = $index;
+        }
+
+        echo "<pre>";
+        print_r($a2);
+        echo "</pre>";
+    */
+
+
+    /* - Exercice 4 - Tableau
+
+    $stage = $a["19003"];
+    $i = 0 ;
+            foreach($stage as $sub_stage)
+            {
+                if($sub_stage == "Stage")
+                {
+                    $i++ ;
+                }
+            }
+    echo $i ;
+    */
+
+
+
+    ?
+    >
